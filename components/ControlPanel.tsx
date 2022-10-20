489bc55753bfcfaa8a27f8f33317b7f7efbf66b4
tohsaka888 <156132264@qq.com>
2022-10-20 09:42:01 +0800

    ✨ feat: 控制面板

--- components/ControlPanel.tsx
+++ components/ControlPanel.tsx
@@ -2,7 +2,9 @@ import { Drawer, Form, Input, Slider } from "antd";
 import React, { useContext, useState } from "react";
 import { BsFillGearFill } from "react-icons/bs";
 import { ConfigContext } from "./ConfigController";
+import GenerateCode from "./GenerateCode";
 import styles from "./index.module.css";
+import TypeConfig from "./TypeConfig";
 
 function ControlPanel() {
   const [open, setOpen] = useState<boolean>(false);
@@ -20,69 +22,79 @@ function ControlPanel() {
         </div>
       )}
       <Drawer
+        className={styles["drawer-container"]}
         title="知识图谱配置"
         placement={"left"}
+        bodyStyle={{
+          padding: "0px",
+        }}
         closable={false}
+        style={{ paddingTop: "0px" }}
         onClose={() => {
           setOpen(false);
         }}
         open={open}
       >
-        <Form
-          initialValues={config}
-          labelCol={{ span: 7 }}
-          wrapperCol={{ span: 17 }}
-        >
-          <Form.Item label={"基础半径"} name={["basicDistence"]}>
-            <Slider
-              min={10}
-              max={100}
-              onChange={(value) => {
-                dispatch({ type: "setBasicDistence", payload: +value });
-              }}
-            />
-          </Form.Item>
-          <Form.Item label={"根节点X坐标"} name={["position", "x"]}>
-            <Input
-              type={"number"}
-              onChange={(e) => {
-                dispatch({
-                  type: "setPosition",
-                  payload: { x: +e.target.value, y: config.position.y },
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item label={"根节点Y坐标"} name={["position", "y"]}>
-            <Input
-              type={"number"}
-              onChange={(e) => {
-                dispatch({
-                  type: "setPosition",
-                  payload: { y: +e.target.value, x: config.position.x },
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item label={"边宽度"} name={["edgeConfig", "strokeWidth"]}>
-            <Slider
-              min={0.1}
-              max={3}
-              step={0.01}
-              onChange={(value) => {
-                dispatch({ type: "setStrokeWidth", payload: +value });
-              }}
-            />
-          </Form.Item>
-          <Form.Item label={"边颜色"} name={["edgeConfig", "stroke"]}>
-            <Input
-              type={"color"}
-              onChange={(e) => {
-                dispatch({ type: "setStroke", payload: e.target.value });
-              }}
-            />
-          </Form.Item>
-        </Form>
+        <div className={styles["drawer-container"]}>
+          <Form
+            initialValues={config}
+            labelCol={{ span: 7 }}
+            wrapperCol={{ span: 17 }}
+          >
+            <div className={styles["form-title"]}>通用配置:</div>
+            <Form.Item label={"基础半径"} name={["basicDistence"]}>
+              <Slider
+                min={10}
+                max={100}
+                onChange={(value) => {
+                  dispatch({ type: "setBasicDistence", payload: +value });
+                }}
+              />
+            </Form.Item>
+            <Form.Item label={"根节点X坐标"} name={["position", "x"]}>
+              <Input
+                type={"number"}
+                onChange={(e) => {
+                  dispatch({
+                    type: "setPosition",
+                    payload: { x: +e.target.value, y: config.position.y },
+                  });
+                }}
+              />
+            </Form.Item>
+            <Form.Item label={"根节点Y坐标"} name={["position", "y"]}>
+              <Input
+                type={"number"}
+                onChange={(e) => {
+                  dispatch({
+                    type: "setPosition",
+                    payload: { y: +e.target.value, x: config.position.x },
+                  });
+                }}
+              />
+            </Form.Item>
+            <Form.Item label={"边宽度"} name={["edgeConfig", "strokeWidth"]}>
+              <Slider
+                min={0.1}
+                max={3}
+                step={0.01}
+                onChange={(value) => {
+                  dispatch({ type: "setStrokeWidth", payload: +value });
+                }}
+              />
+            </Form.Item>
+            <Form.Item label={"边颜色"} name={["edgeConfig", "stroke"]}>
+              <Input
+                type={"color"}
+                onChange={(e) => {
+                  dispatch({ type: "setStroke", payload: e.target.value });
+                }}
+              />
+            </Form.Item>
+            <TypeConfig />
+            <GenerateCode />
+          </Form>
+        </div>
       </Drawer>
     </>
   );
