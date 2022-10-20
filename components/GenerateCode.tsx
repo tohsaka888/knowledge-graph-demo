d62989cd7d0a02131d35455df2255aed3a9193bb
tohsaka888 <156132264@qq.com>
2022-10-20 09:42:57 +0800

    ✨ feat: 生成代码

---
+++ components/GenerateCode.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+import React from "react";
+
+function GenerateCode() {
+  return (
+    <>
+      <Button type={"primary"} style={{ width: "100%" }} >
+        生成代码
+      </Button>
+    </>
+  );
+}
+
+export default GenerateCode;
