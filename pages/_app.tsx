d11741a2bdb449e5519a05faf1e50a7383da5402
tohsaka888 <156132264@qq.com>
2023-01-05 14:20:23 +0800

    🚀 perf: update antd to v5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
