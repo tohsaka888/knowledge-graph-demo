38ed2e09149e25d92427d048a34981b71c7b4411
tohsaka888 <156132264@qq.com>
2023-01-05 14:20:03 +0800

    ♻️ refactor: config baseUrl

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,12 +1,1 @@
-/*
- * @Author: tohsaka888
- * @Date: 2022-10-09 13:39:03
- * @LastEditors: tohsaka888
- * @LastEditTime: 2022-10-09 13:39:04
- * @Description: 请填写简介
- */
-
-export const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://knowledge-graph-demo.netlify.app";
+export const baseUrl = "https://react-knowledge-graph.vercel.app";
