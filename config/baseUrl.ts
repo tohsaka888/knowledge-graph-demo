f3d2927e9fd07e897208cec9fd8614a2310a98e5
tohsaka888 <156132264@qq.com>
2023-04-30 10:03:24 +0800

    🐛 fix: fix react-knowledge-graph bugs

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,1 +1,4 @@
-export const baseUrl = "https://knowledge-graph-demo.tohsaka888.asia";
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://knowledge-graph-demo.tohsaka888.asia";
